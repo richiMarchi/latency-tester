d46084da6379adb3a4753dee3d20447b8aef31c9
Riccardo Marchi <riccardo.marchi4@gmail.com>
2021-02-10 12:52:39 +0100

    improve logging meticulously
    Signed-off-by: Riccardo Marchi <riccardo.marchi4@gmail.com>

--- enhanced-client/enhanced-client.go
+++ enhanced-client/enhanced-client.go
@@ -51,52 +51,64 @@ type Settings struct {
 
 func main() {
 
+	const LoggerHdr = "@main          - "
+
 	if len(os.Args) == 1 {
-		log.Fatal("Settings filename requested")
+		log.Fatal(LoggerHdr + "Settings filename requested")
 	}
 
 	stopHealthChecker := make(chan os.Signal, 1)
-	log.Println("Run Health Checker on '0.0.0.0:8080/health'")
-	go runHealthChecker(stopHealthChecker)
+	log.Println(LoggerHdr + "Run Health Checker on '0.0.0.0:8080/health'")
+	go healthChecker(stopHealthChecker)
 
 	// Settings parsing
+	log.Println(LoggerHdr + "Opening settings file")
 	file, err := ioutil.ReadFile(os.Args[1])
-	errMgmt(err)
+	if err != nil {
+		log.Fatal(LoggerHdr+"*** ERROR opening settings file:", err)
+	} else {
+		log.Println(LoggerHdr + "Settings file successfully opened")
+	}
 	var settings Settings
-	log.Println("Reading settings")
+	log.Println(LoggerHdr + "Reading settings")
 	err = yaml.Unmarshal(file, &settings)
-	errMgmt(err)
+	if err != nil {
+		log.Fatal(LoggerHdr+"*** ERROR reading settings:", err)
+	} else {
+		log.Println(LoggerHdr + "Settings successfully read")
+	}
 
 	if settings.RunsStepDuration == 0 && settings.RunsInterval == 0 {
-		log.Fatal("One between runs_step_duration and runs_interval must be set")
+		log.Fatal(LoggerHdr + "One between runs_step_duration and runs_interval must be set")
 	}
 	combinations := len(settings.Endpoints) * len(settings.Intervals) * len(settings.MsgSizes)
 	if settings.RunsStepDuration == 0 {
 		settings.RunsStepDuration = settings.RunsInterval * 60 / combinations
-		log.Println("Warning: runs_step_duration not set, the value will be", settings.RunsStepDuration)
+		log.Println(LoggerHdr+"Warning: runs_step_duration not set, the value will be", settings.RunsStepDuration)
 	}
 	if settings.RunsInterval == 0 {
 		settings.RunsInterval = int(math.Ceil(float64(settings.RunsStepDuration*combinations) / 60))
-		log.Println("Warning: runs_interval not set, the value will be", settings.RunsInterval)
+		log.Println(LoggerHdr+"Warning: runs_interval not set, the value will be", settings.RunsInterval)
 	}
 	avgSleep := float64(settings.RunsInterval*60-combinations*settings.RunsStepDuration) / 60
 	if avgSleep < 0 {
-		log.Println("Warning: the runs will be out of phase, not enough time to complete a run")
+		log.Println(LoggerHdr + "Warning: the runs will be out of phase, not enough time to complete a run")
 	} else {
-		log.Println("Average sleep minutes between end and start of consecutive runs:", math.Round(avgSleep))
+		log.Println(LoggerHdr+"Average sleep minutes between end and start of consecutive runs:",
+			math.Round(avgSleep))
 	}
 
 	// Print flags statuses in order to be sure it is as expected
 	ss, ssErr := sysctl.Get("net.ipv4.tcp_slow_start_after_idle")
 	cc, ccErr := sysctl.Get("net.ipv4.tcp_congestion_control")
 	if ssErr != nil || ccErr != nil {
-		log.Println("Warning: Cannot access TCP system parameters.")
+		log.Println(LoggerHdr + "Warning: Cannot access TCP system parameters.")
 	} else {
-		log.Println("TCP slow start after idle value: ", ss)
-		log.Println("TCP congestion control algorithm: ", cc)
+		log.Println(LoggerHdr+"TCP slow start after idle value: ", ss)
+		log.Println(LoggerHdr+"TCP congestion control algorithm: ", cc)
 	}
 
-	generateParamsFile(settings)
+	genParamsFile(settings)
 
 	// Start ping and tcpdump in background
 	stopPing := make(chan os.Signal, len(settings.PingDestinations))
@@ -104,7 +116,6 @@ func main() {
 	wg.Add(len(settings.PingDestinations))
 	killPing := false
 	for _, dest := range settings.PingDestinations {
-		log.Println("Starting ping thread towards", dest.Name)
 		go pingThread(&wg, settings.ExecDir, dest, settings.PingInterval, stopPing, &killPing)
 	}
 
@@ -114,17 +125,19 @@ func main() {
 			if iperfData.Port == "" {
 				iperfData.Port = "5201"
 			}
-			log.Println("Running Iperf towards", iperfData.Name+"...")
 			iperfer(i, settings.ExecDir, iperfData)
-			log.Println("Iperf towards", iperfData.Name, "complete!")
 		}
 		// Handle Tcpdump
 		stopTcpdump := make(chan os.Signal, 1)
 		if settings.TcpdumpEnabled {
+			log.Println(LoggerHdr + "Tcpdump is requested")
 			wg.Add(1)
 			localIp := getOutboundIP()
-			log.Println("Starting Tcpdump")
+			log.Println(LoggerHdr + "Outbound IP: " + localIp)
 			go tcpDumper(i, &wg, stopTcpdump, localIp, settings.ExecDir)
+			time.Sleep(time.Second)
+		} else {
+			log.Println(LoggerHdr + "Tcpdump is not requested")
 		}
 		startTime := getTimestamp()
 		// Start E2E analysis
@@ -132,44 +145,53 @@ func main() {
 			for _, inter := range settings.Intervals {
 				for _, size := range settings.MsgSizes {
 					repetitions := int((time.Duration(settings.RunsStepDuration) * time.Second).Milliseconds()) / inter
-					log.Println("Run: " + strconv.Itoa(i) + " - " +
+					log.Println(LoggerHdr + "Run: " + strconv.Itoa(i) + " - " +
 						"EP: " + addr.Destination + " - " +
 						"Inter: " + strconv.Itoa(inter) + " - " +
 						"Msg: " + strconv.Itoa(size))
-					clientCmd := exec.Command("./client", "-reps="+strconv.Itoa(repetitions), "-interval="+strconv.Itoa(inter),
-						"-requestPayload="+strconv.Itoa(size), "-responsePayload="+strconv.Itoa(settings.ResponseSize),
-						"-tls="+settings.TlsEnabled, "-log="+settings.ExecDir+strconv.Itoa(i)+"-"+addr.Destination+
-							".i"+strconv.Itoa(inter)+".x"+strconv.Itoa(size), addr.Destination)
+					clientCmd := exec.Command("./client",
+						"-reps="+strconv.Itoa(repetitions),
+						"-interval="+strconv.Itoa(inter),
+						"-requestPayload="+strconv.Itoa(size),
+						"-responsePayload="+strconv.Itoa(settings.ResponseSize),
+						"-tls="+settings.TlsEnabled,
+						"-log="+settings.ExecDir+strconv.Itoa(i)+"-"+addr.Destination+".i"+strconv.Itoa(inter)+
+							".x"+strconv.Itoa(size), addr.Destination)
 					var stdErrClient bytes.Buffer
 					clientCmd.Stderr = &stdErrClient
 					err = clientCmd.Run()
 					if err != nil {
-						log.Println(err)
+						log.Println(LoggerHdr+"*** ERROR executing client:", err)
+					} else {
+						log.Println(LoggerHdr + "OK! - Client executed successfully")
 					}
 					if stdErrClient.Len() > 0 {
-						log.Println("*** CLIENT ERROR ***\n", stdErrClient.String())
+						log.Println(LoggerHdr+"*** CLIENT STDERR ***\n", stdErrClient.String())
 					}
 				}
 			}
 		}
 		if settings.TcpdumpEnabled {
-			log.Println("Signal Tcpdump Stop")
+			log.Println(LoggerHdr + "Signal Tcpdump Stop")
 			stopTcpdump <- os.Interrupt
+			log.Println(LoggerHdr + "Signal to stop tcpdump successfully received")
 		}
 		if i == settings.Runs {
 			killPing = true
 		}
-		for range settings.PingDestinations {
-			log.Println("Saving gathered ping data to file")
+		for _, pingDst := range settings.PingDestinations {
+			log.Println(LoggerHdr + "Signal " + pingDst.Name + " ping to store gathered data to file")
 			stopPing <- os.Interrupt
+			log.Println(LoggerHdr + "Interrupt to store " + pingDst.Name + " ping data to file successfully received")
 		}
 		if i != settings.Runs {
 			elapsed := getTimestamp().Sub(startTime)
 			waitTime := time.Duration(settings.RunsInterval)*time.Minute - elapsed
 			if waitTime < 0 {
-				log.Println("Warning: Run lasted more than 'run_interval'!")
+				log.Println(LoggerHdr + "Warning: Run lasted more than 'run_interval'!")
 			} else {
-				log.Println("Sleeping for about", strconv.Itoa(int(waitTime.Seconds())), "seconds to wait until next run")
+				log.Println(LoggerHdr+"Sleeping for about", strconv.Itoa(int(waitTime.Seconds())),
+					"seconds to wait until next run")
 				time.Sleep(waitTime)
 			}
 		}
@@ -177,25 +199,32 @@ func main() {
 	wg.Wait()
 
 	// Plotting
-	log.Println("Plotting...")
+	log.Println(LoggerHdr + "Plotting...")
 	plotterCmd := exec.Command("./plotter", os.Args[1])
 	var stdErrPlotter bytes.Buffer
 	plotterCmd.Stderr = &stdErrPlotter
-	err = plotterCmd.Run()
+	plotterOtp, err := plotterCmd.Output()
 	if err != nil {
-		log.Println(err)
+		log.Println(LoggerHdr+"*** ERROR plotting data:", err)
+	} else {
+		log.Println(LoggerHdr+"Plotting successfully executed:\n", string(plotterOtp))
 	}
 	if stdErrPlotter.Len() > 0 {
-		log.Println("*** PLOTTER ERROR ***\n", stdErrPlotter.String())
+		log.Println(LoggerHdr+"*** PLOTTER STDERR ***\n", stdErrPlotter.String())
 	}
 	stopHealthChecker <- os.Interrupt
-	log.Println("Everything's complete!")
+	log.Println(LoggerHdr + "Everything's complete!")
 }
 
-func generateParamsFile(settings Settings) {
-	log.Println("Generating parameters file")
+func genParamsFile(settings Settings) {
+	const LoggerHdr = "@genParamsFile - "
+	log.Println(LoggerHdr + "Generating parameters file")
 	paramsFile, err := os.Create(settings.ExecDir + "parameters.txt")
-	errMgmt(err)
+	if err != nil {
+		log.Fatal(LoggerHdr+"*** ERROR creating parameters file:", err)
+	} else {
+		log.Println(LoggerHdr + "Parameters file successfully created")
+	}
 	defer paramsFile.Close()
 
 	destinations := ""
@@ -224,42 +253,67 @@ func generateParamsFile(settings Settings) {
 	paramsFile.WriteString(sizes)
 }
 
-func runHealthChecker(c chan os.Signal) {
+func healthChecker(c chan os.Signal) {
+	const LoggerHdr = "@healthChecker - "
+
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) { return })
 	server := http.Server{Addr: "0.0.0.0:8080"}
 
 	// Handle stop
 	go func() {
 		for range c {
-			log.Println("Stopping health checker")
+			log.Println(LoggerHdr + "Stopping health checker")
 			err := server.Shutdown(context.TODO())
 			if err != nil {
-				log.Println(err)
+				log.Println(LoggerHdr+"*** ERROR stopping health checker:", err)
+			} else {
+				log.Println(LoggerHdr + "Health checker successfully stopped")
 			}
+			break
 		}
 	}()
 
+	log.Println(LoggerHdr + "Health checker listening...")
 	if err := server.ListenAndServe(); err != http.ErrServerClosed {
-		log.Fatalf("ListenAndServe(): %v", err)
+		log.Fatal(LoggerHdr+"*** ERROR in ListenAndServe():", err)
 	}
 }
 
 func iperfer(run int, execdir string, iperfData IperfData) {
+	const LoggerHdr = "@iperfer       - "
+
+	log.Println(LoggerHdr + "Creating " + iperfData.Name + " iperf file")
 	iperfFile, err := os.Create(execdir + strconv.Itoa(run) + "-iperf_" + iperfData.Name + ".txt")
-	errMgmt(err)
+	if err != nil {
+		log.Fatal(LoggerHdr+"*** ERROR creating "+iperfData.Name+" iperf file:", err)
+	} else {
+		log.Println(LoggerHdr + iperfData.Name + " iperf file successfully created")
+	}
 	defer iperfFile.Close()
 
+	log.Println(LoggerHdr+"Running Iperf towards", iperfData.Name+"...")
 	iperfRes, err := exec.Command(
 		"timeout", "10", "iperf3", "-c", iperfData.Ip, "-p", iperfData.Port, "-t", "5").Output()
+	log.Println(LoggerHdr+"Iperf towards", iperfData.Name, "complete!")
+	log.Println(LoggerHdr + "Saving " + iperfData.Name + " iperf output to file")
 	_, err = iperfFile.Write(iperfRes)
 	if err != nil {
-		log.Println(err)
+		log.Println(LoggerHdr+"*** ERROR writing "+iperfData.Name+" iperf output to file", err)
+	} else {
+		log.Println(LoggerHdr + iperfData.Name + " iperf output successfully saved to file")
 	}
 }
 
 func pingThread(wg *sync.WaitGroup, execdir string, destination PingData, interval int, c chan os.Signal, kill *bool) {
+	const LoggerHdr = "@pingThread    - "
+
+	log.Println(LoggerHdr + "Creating " + destination.Name + " ping file")
 	osRtt, err := os.Create(execdir + "ping_" + destination.Name + ".txt")
-	errMgmt(err)
+	if err != nil {
+		log.Fatal(LoggerHdr+"*** ERROR creating "+destination.Name+" ping file:", err)
+	} else {
+		log.Println(LoggerHdr + destination.Name + " ping file successfully created")
+	}
 	defer osRtt.Close()
 	for !*kill {
 		pingerCmd := exec.Command("ping", destination.Ip, "-i", strconv.Itoa(interval), "-D")
@@ -267,29 +321,45 @@ func pingThread(wg *sync.WaitGroup, execdir string, destination PingData, interv
 		// Handle stop
 		go func() {
 			for range c {
-				log.Println("Stopping ping to save to file")
+				log.Println(LoggerHdr + "Stopping " + destination.Name + " ping to save to file")
 				err = pingerCmd.Process.Signal(os.Interrupt)
 				if err != nil {
-					log.Println(err)
+					log.Println(LoggerHdr+"*** ERROR stopping "+destination.Name+" ping:", err)
+				} else {
+					log.Println(LoggerHdr + destination.Name + " ping successfully stopped")
 				}
+				break
 			}
 		}()
 
+		log.Println(LoggerHdr+"Starting ping towards", destination.Name)
 		pingOutput, err := pingerCmd.Output()
 		if err != nil {
-			log.Println(err)
+			log.Println(LoggerHdr+"*** ERROR pinging "+destination.Name+":", err)
+		} else {
+			log.Println(LoggerHdr + destination.Name + " ping successfully executed")
 		}
+		log.Println(LoggerHdr + "Saving to file " + destination.Name + " ping output")
 		_, err = osRtt.Write(pingOutput)
 		if err != nil {
-			log.Println(err)
+			log.Println(LoggerHdr+"*** ERROR saving "+destination.Name+" ping output to file:", err)
+		} else {
+			log.Println(LoggerHdr + destination.Name + " ping data successfully saved to file")
 		}
 	}
 	wg.Done()
 }
 
 func tcpDumper(run int, wg *sync.WaitGroup, c chan os.Signal, localIp, execdir string) {
+	const LoggerHdr = "@tcpDumper     - "
+
+	log.Println(LoggerHdr + "Creating tcpdump output file for run " + strconv.Itoa(run))
 	tcpRtt, err := os.Create(execdir + strconv.Itoa(run) + "-tcpdump_report.csv")
-	errMgmt(err)
+	if err != nil {
+		log.Fatal(LoggerHdr+"*** ERROR creating tcpdump output file for run "+strconv.Itoa(run), err)
+	} else {
+		log.Println(LoggerHdr + "Tcpdump output file for run " + strconv.Itoa(run) + " successfully created")
+	}
 	defer tcpRtt.Close()
 	tcpRtt.WriteString("#frame-timestamp,tcp-ack-rtt,tcp-stream-id,retransmission\n")
 	tcpdumpCmd := exec.Command("tshark",
@@ -306,21 +376,30 @@ func tcpDumper(run int, wg *sync.WaitGroup, c chan os.Signal, localIp, execdir s
 	// Handle stop
 	go func() {
 		for range c {
-			log.Println("Stop Tcpdump")
+			log.Println(LoggerHdr + "Stopping tcpdump for run " + strconv.Itoa(run))
 			err = tcpdumpCmd.Process.Signal(os.Interrupt)
 			if err != nil {
-				log.Println(err)
+				log.Println(LoggerHdr+"*** ERROR stopping tcpdump for run "+strconv.Itoa(run)+":", err)
+			} else {
+				log.Println(LoggerHdr + "Tcpdump for run " + strconv.Itoa(run) + " successfully stopped")
 			}
+			break
 		}
 	}()
 
+	log.Println(LoggerHdr + "Starting tcpdump for run " + strconv.Itoa(run))
 	tcpdumpOutput, err := tcpdumpCmd.Output()
 	if err != nil {
-		log.Println(err)
+		log.Println(LoggerHdr+"*** ERROR executing tcpdump for run "+strconv.Itoa(run)+":", err)
+	} else {
+		log.Println(LoggerHdr + "Tcpdump for run " + strconv.Itoa(run) + " successfully executed")
 	}
+	log.Println(LoggerHdr + "Saving tcpdump output for run " + strconv.Itoa(run) + " to file")
 	_, err = tcpRtt.Write(tcpdumpOutput)
 	if err != nil {
-		log.Println(err)
+		log.Println(LoggerHdr+"*** ERROR saving tcpdump output for run "+strconv.Itoa(run)+" to file", err)
+	} else {
+		log.Println(LoggerHdr + "Tcpdump output for run " + strconv.Itoa(run) + " successfully saved to file")
 	}
 	wg.Done()
 }
@@ -329,17 +408,16 @@ func getTimestamp() time.Time {
 	return time.Now()
 }
 
-func errMgmt(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
 // Get preferred outbound ip of this machine
 func getOutboundIP() string {
+	const LoggerHdr = "@getOutboundIP - "
+
+	log.Println(LoggerHdr + "Retrieving default outbound IP")
 	conn, err := net.Dial("udp", "8.8.8.8:80")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(LoggerHdr+"*** ERROR dialing to understand default outbound IP:", err)
+	} else {
+		log.Println(LoggerHdr + "Outbound IP successfully retrieved")
 	}
 	defer conn.Close()
 
